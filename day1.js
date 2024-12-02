ee0639447fb2ac0a5bde3d2f9d9d5d2ca545d28e
zohra101 <alex.marjanovic.ba.tw@gmail.com>
2024-12-02 11:33:16 -0600

    Practice 1

---
+++ day1.js
@@ -0,0 +1,27 @@
+function practice1() {
+   
+    const partyGuest = {
+        guestName: "Alex",
+        numAttendees: 1,
+        rsvp: "yes"       
+    }
+    document.write("Practice 1:  " + partyGuest.guestName + ", " + partyGuest.numAttendees + ", " + partyGuest.rsvp);
+}
+
+function practice2() {
+    debugger;
+   const partyGuest1 = ["Alex", 1, "yes"]
+   let index = 0;
+
+ for  (len = partyGuest1.length; 
+        index = len;
+        index++)
+      
+    // {   let partyGuestInfo = "";
+        const partyGuestInfo += partyGuest1[index] + ", "; 
+        return partyGuestInfo;
+        // document.write(partyGuestInfo);
+    }
+
+}
+ 

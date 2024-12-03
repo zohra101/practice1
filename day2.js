48c58c3e9d4a9b1b0470cccfe5f28e6cb3c9a336
zohra101 <alex.marjanovic.ba.tw@gmail.com>
2024-12-03 13:28:04 -0600

    Practice 2

--- day2.js
+++ day2.js
@@ -1,15 +1,17 @@
 function practice1() {
-book = {title: "Dragonsong", authorLast: "McCaffrey", authorFirst:"Anne", inStock: true, quantity: 2};
+const book = ["Dragonsong", "McCaffrey","Anne", true, 2];
 
 let index=0;
-let book = "";
+let bookInfo="";
 
 for (let len = book.length; index < len; index++) {
-    document.write(book[index]+ "<br>");
+    bookInfo += book[index] + "<br>";
     }
+    document.write("Practice 1:<br>" + bookInfo);
 }
 
-function practice1() {
+function practice2() {
+    debugger;
 const book1 = {title: "Dragonsong", authorLast: "McCaffrey", authorFirst:"Anne", inStock: true, quantity: 2};
 const book2 = {title: "Dragonsinger", authorLast: "McCaffrey", authorFirst:"Anne", inStock: true, quantity: 3};
 const book3 = {title: "Dragondrums", authorLast: "McCaffrey", authorFirst:"Anne", inStock: false, quantity: 0};
@@ -20,7 +22,10 @@ const books = [book1, book2, book3]
     let bookInfo = "";
     
     for (let len = books.length; index < len; index++) {
-        bookInfo += `Name: ${books[index].title}<br> Author: ${books[index].authorLast}, ${books[index].authorFirst}<br>In Stock: ${partyGuests[index].inStock}<br>Quantity: ${boooks[index].quantity}<br><br>`;
+        bookInfo += `Name: ${books[index].title}<br> 
+        Author: ${books[index].authorLast}, ${books[index].authorFirst}<br>
+        In Stock: ${books[index].inStock}<br>
+        Quantity: ${boooks[index].quantity}<br><br>`;
         }
     document.write(bookInfo);
 }

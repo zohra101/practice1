9b81fe58ffbc6829f16c17e481f0736ffc2ea972
zohra101 <alex.marjanovic.ba.tw@gmail.com>
2024-12-15 15:19:52 -0600

    Practice Day 4

---
+++ day4.js
@@ -0,0 +1,53 @@
+function handleSubmit(event = new Event()) {
+    event.preventDefault();
+
+    const inputs = event.target;
+
+    const greenInput = inputs[0];
+    const purpleInput = inputs[1];
+    const redInput = inputs[2];
+    const blueInput = inputs[3];
+
+    const greenChecked = greenInput.checked;
+    const purpleChecked = purpleInput.checked;
+    const redChecked = redInput.checked;
+    const blueChecked = blueInput.checked;
+
+    output(`green: ${greenChecked}<br>`);
+    output(`purple: ${purpleChecked}<br>`);
+    output(`red: ${redChecked}<br>`);
+    output(`blue: ${blueChecked}<br>`);
+
+    debugger;
+
+    const count = inputs.length;
+    for (let i=0; i < count; i++) {
+        const radioInput = inputs[i];
+        const name = radioInput.name;
+        output(`${radioInput.name}<br>`);
+    }
+
+    for (let i=0; i < count; i++) {
+        const radioInput = inputs[i];
+        const value = radioInput.value;
+        output(`${radioInput.value}<br>`);
+    }
+
+    for (let i=0; i < count; i++) {
+        const radioInput = inputs[i];
+        const value = radioInput.value;
+        const isChecked = radioInput.checked;
+        if (isChecked) output(`${value}<br>`);   
+        }
+
+    for (let i=0; i < count; i++) {
+        const radioInput = inputs[i];
+        const value = radioInput.value;
+        const isChecked = radioInput.checked;
+        const isDatatype = radioInput.name;
+
+        if (isDatatype = "datatype") {
+            if (isChecked) output(`${value}<br>`); 
+        }
+    }
+ }
